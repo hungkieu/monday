0a5f420a5de7090713298e6e447b681e295e36cb
duccuongdc97 <cuong.hoangduc@meditech.vn>
2019-05-27 00:14:30 +0700

    admin-login-register

--- models/users.models.js
+++ models/users.models.js
@@ -6,6 +6,6 @@ const userSchema = new mongoose.Schema({
   password: { type: String, trim: true }
 })
 
-const User = mongoose.model('User', userSchema, 'users')
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
